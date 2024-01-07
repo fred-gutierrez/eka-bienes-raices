8fda0602d59731a9351f8ee7cf3b7fd78e1437a2
fred-gutierrez <fredsg22@hotmail.com>
2024-01-06 22:09:28 -0600

    Fixed the pagination overflow by only showing 7 per page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,20 +13,56 @@ export default function Pagination({
   setCurrentPage,
   currentPage,
 }: Props) {
-  let pages = [];
+  const totalPages = Math.ceil(totalPosts / postsPerPage);
+  const pageRange = 7;
+  let pages: number[] = [];
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+  for (let i = 1; i <= totalPages; i++) {
     pages.push(i);
   }
+
+  const renderPageLinks = () => {
+    const buttons = [];
+    const halfPageRange = Math.floor(pageRange / 2);
+    const startPage =
+      currentPage - halfPageRange > 0 ? currentPage - halfPageRange : 1;
+    const endPage =
+      startPage + pageRange - 1 <= totalPages
+        ? startPage + pageRange - 1
+        : totalPages;
+
+    const adjustedStartPage =
+      startPage > totalPages - pageRange ? totalPages - pageRange + 1 : startPage;
+
+    for (let i = adjustedStartPage; i <= endPage; i++) {
+      buttons.push(
+        <Link
+          key={i}
+          onClick={() => setCurrentPage(i)}
+          href={`/propiedades/${i}`}
+          className={`px-3 py-2.5 leading-tight h-10
+            ${i === currentPage
+              ? `dark:text-orange-500 dark:border-orange-500 dark:bg-orange-900 dark:hover:bg-orange-800
+                 text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-70 font-bold`
+              : `dark:text-neutral-400 dark:border-neutral-400 dark:hover:text-orange-500 dark:bg-neutral-700
+                 text-gray-500 border-gray-300 hover:bg-gray-100 hover:text-gray-700 bg-white border`
+            }`}
+        >
+          {i}
+        </Link>
+      );
+    }
+
+    return buttons;
+  };
 
   return (
     <nav className="text-center" aria-label="Page navigation">
       <div className="flex items-center justify-center">
         <Link
           onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)}
-          href={`/propiedades/${
-            currentPage > 1 ? currentPage - 1 : currentPage
-          }`}
+          href={`/propiedades/${currentPage > 1 ? currentPage - 1 : currentPage
+            }`}
           className={`
             dark:bg-neutral-700 dark:text-white dark:border-neutral-400 dark:hover:bg-neutral-600
             text-gray-500 border-gray-300 hover:bg-gray-100 hover:text-gray-700
@@ -36,34 +72,34 @@ export default function Pagination({
           <i className="fa-solid fa-chevron-left"></i>
         </Link>
 
-        {pages.map((page, index) => (
-          <Link
-            key={index}
-            onClick={() => setCurrentPage(page)}
-            href={`/propiedades/${page}`}
-            className={`px-3 py-2.5 leading-tight h-10
-              ${
-                page === currentPage
-                  ? `dark:text-orange-500 dark:border-orange-500 dark:bg-orange-900 dark:hover:bg-orange-800
-                     text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-70 font-bold`
-                  : `dark:text-neutral-400 dark:border-neutral-400 dark:hover:text-orange-500 dark:bg-neutral-700
-                     text-gray-500 border-gray-300 hover:bg-gray-100 hover:text-gray-700 bg-white border`
-              }`}
-          >
-            {page}
-          </Link>
-        ))}
+        {renderPageLinks()}
+
+        {/* {pages.map((page, index) => ( */}
+        {/*   <Link */}
+        {/*     key={index} */}
+        {/*     onClick={() => setCurrentPage(page)} */}
+        {/*     href={`/propiedades/${page}`} */}
+        {/*     className={`px-3 py-2.5 leading-tight h-10 */}
+        {/*       ${page === currentPage */}
+        {/*         ? `dark:text-orange-500 dark:border-orange-500 dark:bg-orange-900 dark:hover:bg-orange-800 */}
+        {/*              text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-70 font-bold` */}
+        {/*         : `dark:text-neutral-400 dark:border-neutral-400 dark:hover:text-orange-500 dark:bg-neutral-700 */}
+        {/*              text-gray-500 border-gray-300 hover:bg-gray-100 hover:text-gray-700 bg-white border` */}
+        {/*       }`} */}
+        {/*   > */}
+        {/*     {page} */}
+        {/*   </Link> */}
+        {/* ))} */}
 
         <Link
           onClick={() =>
             currentPage < Math.ceil(totalPosts / postsPerPage) &&
             setCurrentPage(currentPage + 1)
           }
-          href={`/propiedades/${
-            currentPage < Math.ceil(totalPosts / postsPerPage)
-              ? currentPage + 1
-              : currentPage
-          }`}
+          href={`/propiedades/${currentPage < Math.ceil(totalPosts / postsPerPage)
+            ? currentPage + 1
+            : currentPage
+            }`}
           className={`
             dark:bg-neutral-700 dark:text-white dark:border-neutral-400 dark:hover:bg-neutral-600
             text-gray-500 border-gray-300 hover:bg-gray-100 hover:text-gray-700
